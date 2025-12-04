b7f5ec72e25c33aae06e2c4f898d0850bbd3adac
hajos8 <rabb.andras@gmail.com>
2025-12-04 08:37:06 +0100

    Implement GET

--- react-app/api/neon-cats.js
+++ react-app/api/neon-cats.js
@@ -1,3 +1,13 @@
+import { neon } from '@neondatabase/serverless';
+
 export default async function handler(req, res) {
+    const conn = neon(process.env.DATABASE_URL);
+
+    if (req.method === 'GET') {
+        const result = await conn`SELECT id, name, isMale, age FROM cats LIMIT 20;`;
+        return res.status(200).json(result);
+    }
+
+
     return res.status(200).json('neon-cats api is working');
 }
