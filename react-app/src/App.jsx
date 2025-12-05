d7f5f2e060489914cbfda7cd2363e7a7dda94efe
hajos8 <rabb.andras@gmail.com>
2025-12-05 11:07:22 +0100

    Added firebase

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 import Cats from './Cats'
-import Gypsy from './Gypsy'
+//import Gypsy from './Gypsy'
+import Users from './Users'
 
 function App() {
 
   return (
     <>
+      {/*}
       <h1>Cats</h1>
       <Cats />
       <h1>Gypsy</h1>
       <Gypsy />
+      */}
+      <h1>Users</h1>
+      <Users />
     </>
   )
 }
